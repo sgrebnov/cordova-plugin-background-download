b56f1c1e6dd8bfeb3d918142f13929bc2232032f
sgrebnov <sergei.grebnov@gmail.com>
2014-01-09 22:05:47 +0400

    adds plugin signature and short info

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 cordova-plugin-background-download
 ==================================
+
+Performs asynchronous resource download operation on a regular basis. Plugin is designed for background work so operates even when the app is inactive; when the user launches or re-opens the app, it can start up with the most current data possible.
+
+**Sample usage**
+
+        var backgroundDownload = BackgroundDownloader.createDownload("uri", "fileName", 30);
+
+        backgroundDownload.startAsync(function () {
+            console.log('successfully started');
+        },
+        function(err) {
+            console.log('error: ' + err);
+        });
+        
+
+        backgroundDownload.stop(function () {
+            console.log('successfully stopped');
+        },
+        function (err) {
+            console.log('error: ' + err);
+        });
+
+
+Links to corresponsing WinRT Api (TODO: remove)
+
+http://msdn.microsoft.com/en-us/library/windows/apps/windows.networking.backgroundtransfer.backgrounddownloader.aspx
+http://msdn.microsoft.com/en-us/library/windows/apps/windows.networking.backgroundtransfer.downloadoperation.aspx
---
+++ www/DownloadOperation.js
@@ -0,0 +1,59 @@
+﻿/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+var exec = require('cordova/exec');
+
+/**
+ * Performs recurrent asynchronous background download operations on a regular basis.
+*/
+var DownloadOperation = function (uri, resultFile, minTimeInterval) {
+
+    this.uri = uri;
+    this.resultFile = resultFile;
+    this.minTimeInterval = minTimeInterval;
+    // generated id is used to make sure background download for this resource is not scheduled twice
+    this.id = uri + '|' + location;
+
+};
+
+/**
+ * Starts download operations.
+*/
+DownloadOperation.prototype.startAsync = function(successCallback, errorCallback) {
+
+    if (typeof this.uri === "undefined" || typeof this.resultFile === "undefined") {
+        errorCallback("invalid argument");
+        return;
+    }
+    
+    exec(successCallback, errorCallback, "DownloadOperation", "startAsync", [this]);
+};
+
+/**
+ * Stops download operations.
+*/
+DownloadOperation.prototype.stop = function(successCallback, errorCallback) {
+
+    exec(successCallback, errorCallback, "DownloadOperation", "stop", [this]);
+
+};
+
+module.exports = DownloadOperation;
