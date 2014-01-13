ff347e9ac7dda50d7e1fd83466831795d5379a38
sgrebnov <sergei.grebnov@gmail.com>
2014-01-13 20:47:08 +0400

    makes wp8 version to work in background

--- src/wp/BackgroundDownload.cs
+++ src/wp/BackgroundDownload.cs
@@ -1,41 +1,58 @@
 ﻿using System;
-using System.IO;
 using System.IO.IsolatedStorage;
-using System.Net;
-using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.Phone.BackgroundTransfer;
 
 namespace WPCordovaClassLib.Cordova.Commands
 {       
     /// <summary>
     /// TODO comments
     /// TODO concurrent operations support
-    /// Throws cancelled if application goes to background.
-    /// Possible workaround
-    /// Try BackgroundTransfer Api
-    /// http://msdn.microsoft.com/en-us/library/windowsphone/develop/hh202955%28v=vs.105%29.aspx#BKMK_TheBackgroundTransferAPIs
-    /// Or custom logic: read data from server by chunks via custom implementation
     /// </summary>
     class BackgroundDownload : BaseCommand
     {
         private string _uri;
         private string _targetFilePath;
-        private WebClient _webClient;
         private string _callbackId;
+        private BackgroundTransferRequest _transfer;
 
         public void startAsync(string options)
         {
             try
             {
                 var optStings = JSON.JsonHelper.Deserialize<string[]>(options);
-                _uri = optStings[0]+"?key=" + Guid.NewGuid();
+                _uri = optStings[0];
                 _targetFilePath = optStings[1];
                 _callbackId = optStings[2];
 
-                _webClient = new WebClient();
-                _webClient.OpenReadCompleted += DowloadCompleted;
-                _webClient.DownloadProgressChanged += ProgressChanged;
+                var requestUri = new Uri(_uri);
 
-                _webClient.OpenReadAsync(new Uri(_uri));
+                _transfer = FindTransfer(_targetFilePath);
+
+                if (_transfer == null)
+                {
+                    // "shared\transfers" is the only working location for BackgroundTransferService download
+                    // we use temporary file name to download content and then move downloaded file to the right location
+                    var downloadLocation = new Uri(@"\shared\transfers\" + Guid.NewGuid(), UriKind.Relative);
+
+                    _transfer = new BackgroundTransferRequest(requestUri, downloadLocation);
+
+                    // Tag is used to make sure we run single background transfer for this file
+                    _transfer.Tag = _targetFilePath;
+
+                    BackgroundTransferService.Add(_transfer);
+                }
+
+                if (_transfer.TransferStatus == TransferStatus.Completed)
+                {
+                    DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
+                    BackgroundTransferService.Remove(_transfer);
+                }
+                else
+                {
+                    _transfer.TransferProgressChanged += ProgressChanged;
+                    _transfer.TransferStatusChanged += TransferStatusChanged;
+                }
                 
             }
             catch (Exception ex)
@@ -44,13 +61,86 @@ namespace WPCordovaClassLib.Cordova.Commands
             }
         }
 
+        private void MoveFile()
+        {
+            // The downloaded content is moved into the right place
+            using (var isoStore = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                string filename = _targetFilePath;
+                if (isoStore.FileExists(filename))
+                {
+                    isoStore.DeleteFile(filename);
+                }
+                isoStore.MoveFile(_transfer.DownloadLocation.OriginalString, filename);
+            }
+        }
+
+        private void CleanUp()
+        {
+            if (_transfer != null)
+            {
+                _transfer.TransferProgressChanged -= ProgressChanged;
+                _transfer.TransferStatusChanged -= TransferStatusChanged;
+
+                if (BackgroundTransferService.Find(_transfer.RequestId) != null)
+                {
+                    BackgroundTransferService.Remove(_transfer);
+                }
+
+                _transfer = null;
+            }
+        }
+
+        private void TransferStatusChanged(object sender, BackgroundTransferEventArgs backgroundTransferEventArgs)
+        {
+            var transfer = backgroundTransferEventArgs.Request;
+
+            if (transfer.TransferStatus == TransferStatus.Completed)
+            {
+                // If the status code of a completed _transfer is 200 or 206, the _transfer was successful
+                if (transfer.StatusCode == 200 || transfer.StatusCode == 206)
+                {
+                    MoveFile();
+                    DispatchCommandResult(new PluginResult(PluginResult.Status.OK), _callbackId);
+                    
+                }
+                else
+                {
+                    var strErrorMessage = transfer.TransferError != null ? transfer.TransferError.Message : "Unspecified transfer error";
+                    DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, strErrorMessage), _callbackId);
+
+                }
+                CleanUp();
+            }
+        }
+
+        private static BackgroundTransferRequest FindTransfer(string tag)
+        {
+            return BackgroundTransferService.Requests.FirstOrDefault(r => r.Tag == tag);
+        }
+
+        void ProgressChanged(object sender, BackgroundTransferEventArgs e)
+        {
+            var progressUpdate = new PluginResult(PluginResult.Status.OK);
+
+            progressUpdate.KeepCallback = true;
+            progressUpdate.Message = String.Format("{{\"progress\":{0}}}", 100 * e.Request.BytesReceived / e.Request.TotalBytesToReceive);
+
+            DispatchCommandResult(progressUpdate, _callbackId);
+        }
+
         public  void stop(string options)
         {
             try
             {
-                if (_webClient != null)
+
+                if (_transfer != null)
                 {
-                    _webClient.CancelAsync();
+                    var request = BackgroundTransferService.Find(_transfer.RequestId);
+                    if (request != null)
+                    {
+                        BackgroundTransferService.Remove(request);
+                    }
                 }
 
                 DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
@@ -60,68 +150,5 @@ namespace WPCordovaClassLib.Cordova.Commands
                 DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
             }
         }
-
-        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            
-            var progressUpdate = new PluginResult(PluginResult.Status.OK);
-
-            progressUpdate.KeepCallback = true;
-            progressUpdate.Message = String.Format("{{\"progress\":{0}}}", e.ProgressPercentage);
-
-            DispatchCommandResult(progressUpdate, _callbackId);
-        }
-
-        private async void DowloadCompleted(object sender, OpenReadCompletedEventArgs e)
-        {
-            try
-            {
-                if (e.Cancelled)
-                {
-                    DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, "Cancelled"), _callbackId);
-                    return;
-                }
-
-                if (e.Error != null)
-                {
-                    DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, e.Error.Message), _callbackId);
-                    return;
-                }
-
-                await SaveToLocalFolderAsync(e.Result, _targetFilePath);
-
-                DispatchCommandResult(new PluginResult(PluginResult.Status.OK), _callbackId);
-
-            }
-            catch (Exception ex)
-            {
-                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, ex.Message));
-            }
-            finally
-            {
-                CleanUp();
-            }
-        }
-
-        private async Task SaveToLocalFolderAsync(Stream file, string fileName)
-        {
-            using (IsolatedStorageFile isoFile = IsolatedStorageFile.GetUserStoreForApplication())
-            {
-                using (Stream outputStream = isoFile.CreateFile(fileName))
-                {
-                    await file.CopyToAsync(outputStream);
-                }
-            }
-        }
-
-        private void CleanUp()
-        {
-            if (_webClient == null) return;
-            
-            _webClient.OpenReadCompleted -= DowloadCompleted;
-            _webClient.DownloadProgressChanged -= ProgressChanged;
-            _webClient = null;
-        }
-
     }
 }
