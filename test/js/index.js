4038ac7a58690e45ec875cd31bffe1880e6c35da
sgrebnov <sergei.grebnov@gmail.com>
2014-01-16 12:34:10 +0400

    sample. adds 'file info' button

--- test/js/index.js
+++ test/js/index.js
@@ -17,6 +17,9 @@
  * under the License.
  */
 var app = {
+
+    fileName: "PointerEventsCordovaPlugin.wmv",
+    uriString: "http://media.ch9.ms/ch9/8c03/f4fe2512-59e5-4a07-bded-124b06ac8c03/PointerEventsCordovaPlugin.wmv",  // 38.3 MB
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -25,9 +28,6 @@ var app = {
     downloadFile: function(uriString, targetFile) {
 
         var lblProgress = document.getElementById('lblProgress');
-
-        // show that download has been started
-        lblProgress.innerHTML = '0%';
 
         var complete = function() {
             lblProgress.innerHTML = 'Done';
@@ -47,12 +47,8 @@ var app = {
             var download = downloader.createDownload(uriString, targetFile);
             // Start the download and persist the promise to be able to cancel the download.
             app.downloadPromise = download.startAsync();
+            
             app.downloadPromise.then(complete, error, progress);
-
-            // TODO Q.js does not support cancel method for promises so our custom cancel method is missed
-            // at promises chaining; this is why we save original promise and not the one returned after .then();
-            // download.startAsync().cancel exists
-            // download.startAsync().then(complete, error, progress).cancel does not exist
 
         } catch(err) {
             console.log('Error: ' + err);
@@ -68,6 +64,7 @@ var app = {
 
         document.getElementById('btnStart').addEventListener('click', this.startDownload);
         document.getElementById('btnStop').addEventListener('click', this.stopDownload);
+        document.getElementById('btnFileInfo').addEventListener('click', this.getFileInfo);
     },
     // deviceready Event Handler
     //
@@ -75,22 +72,35 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-    },
+
+        app.startDownload();
+    },   
 
     startDownload: function () {
-
-        var fileName = "PointerEventsCordovaPlugin.wmv", 
-            uriString = "http://media.ch9.ms/ch9/8c03/f4fe2512-59e5-4a07-bded-124b06ac8c03/PointerEventsCordovaPlugin.wmv"; // 38.3 MB test file
-
+        
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
-            fileSystem.root.getFile(fileName, { create: true }, function (newFile) {
-                app.downloadFile(uriString, newFile);
+            fileSystem.root.getFile(app.fileName, { create: true }, function (newFile) {
+                app.downloadFile(app.uriString, newFile);
             });
         });
     },
     
     stopDownload: function () {
         app.downloadPromise && app.downloadPromise.cancel();
+    },
+    
+    getFileInfo: function () {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+            fileSystem.root.getFile(app.fileName, { create: true }, function (fileEntry) {
+                fileEntry.file(function (meta) {
+                    document.getElementById('lblFileInfo').innerHTML =
+                        "Modified: " + meta.lastModifiedDate + "<br/>" +
+                        "size: " + meta.size;
+                });
+            }, function(error) {
+                document.getElementById('lblFileInfo').innerHTML = "error: " + error;
+            });
+        });
     },
 
         // Update DOM on a Received Event
