a91607a876625c45526fa49cb819a3bc16c05f46
sgrebnov <sergei.grebnov@gmail.com>
2014-01-21 23:52:05 +0400

    windows8. adds concurrent download operations support

--- src/windows8/BackgroundDownloadProxy.js
+++ src/windows8/BackgroundDownloadProxy.js
@@ -1,5 +1,4 @@
 module.exports = {
-    downloadOperationPromise: null, // TODO concurrent operations support
     startAsync: function (success, fail, args) {
         try {
             var uri = new Windows.Foundation.Uri(args[0]),
@@ -21,15 +20,14 @@ module.exports = {
                 });
             };
 
-            var downloadLocation = null;
-            Windows.Storage.StorageFile.getFileFromPathAsync(resultFilePath).then(
+            var downloadLocation;
+            
+            var downloadPromise = Windows.Storage.StorageFile.getFileFromPathAsync(resultFilePath).then(
                 function (file) {
                     downloadLocation = file;
                 }, errorHandler).then(function () {
                     return Windows.Networking.BackgroundTransfer.BackgroundDownloader.getCurrentDownloadsAsync();
                 }, errorHandler).then(function (downloads) {
-                    var downloadOperation = null;
-
                     // After app termination, an app should enumerate all existing DownloadOperation instances at next start-up using
                     // GetCurrentDownloadsAsync. When a Windows Store app using Background Transfer is terminated, incomplete downloads 
                     // will persist in the background. If the app is restarted after termination and operations from the previous
@@ -37,19 +35,20 @@ module.exports = {
                     // http://msdn.microsoft.com/library/windows/apps/br207126
                     for (var i = 0; i < downloads.size; i++) {
                         if (downloads[i].requestedUri.absoluteUri == uri.absoluteUri) {
-                            downloadOperationPromise = downloads[i].attachAsync();
-                            return downloadOperationPromise;
+                            // resume download
+                            return downloads[i].attachAsync();
                         }
-
                     }
-
                     // new download
-                    downloadOperation = Windows.Networking.BackgroundTransfer.BackgroundDownloader().createDownload(uri, downloadLocation);
-                    downloadOperationPromise = downloadOperation.startAsync();
+                    return Windows.Networking.BackgroundTransfer.BackgroundDownloader().createDownload(uri, downloadLocation).startAsync();
+                }, errorHandler);
 
-                    return downloadOperationPromise;
+            // attach callbacks
+            downloadPromise.then(completeHandler, errorHandler, progressHandler);
+            // save operation promise to be able to stop it later
+            BackgroundTransfer.activeDownloads = BackgroundTransfer.activeDownloads || [];
+            BackgroundTransfer.activeDownloads[uri.absoluteUri] = downloadPromise;
 
-                }, errorHandler).then(completeHandler, errorHandler, progressHandler);
         } catch(ex) {
             fail(ex);
         }
@@ -57,10 +56,18 @@ module.exports = {
     },
     stop: function (success, fail, args) {
         try {
-            downloadOperationPromise && downloadOperationPromise.cancel();
+            var uri = new Windows.Foundation.Uri(args[0]);
+
+            if (BackgroundTransfer.activeDownloads && BackgroundTransfer.activeDownloads[uri.absoluteUri]) {
+                BackgroundTransfer.activeDownloads[uri.absoluteUri].cancel();
+                BackgroundTransfer.activeDownloads[uri.absoluteUri] = null;
+            }
+
             success();
+
         } catch(ex) {
             fail(ex);
         }
     }
 };
+require("cordova/windows8/commandProxy").add("BackgroundDownload", module.exports);
