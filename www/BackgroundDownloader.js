5eb1db3cc27c3c823eeb8cd1f3de55c4b56f14a0
Errietta Kostala <errietta@users.noreply.github.com>
2015-12-26 21:03:56 +0000

    Update BackgroundDownloader.js

--- www/BackgroundDownloader.js
+++ www/BackgroundDownloader.js
@@ -34,9 +34,10 @@ var BackgroundDownloader = function() {
  *
  * @param {string} uri The location of the resource.
  * @param {File} resultFile The file that the response will be written to.
+ * @param {string} appTitle The title of the app, which will be shown in notification
  */
-BackgroundDownloader.prototype.createDownload = function(uri, resultFile) {
-    return new DownloadOperation(uri, resultFile);
+BackgroundDownloader.prototype.createDownload = function(uri, resultFile, appTitle) {
+    return new DownloadOperation(uri, resultFile, appTitle);
 };
 
 module.exports = BackgroundDownloader;
